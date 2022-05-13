fe17cde944c207322a3b6d85f2f69ea13bf0dbf3
Henry Lee <Henry840220@gmail.com>
2022-05-13 16:56:36 +0800

    加入axios && 調整eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,10 @@ module.exports = {
     "plugin:vue/vue3-recommended",
     "eslint:recommended"
   ],
+  "rules": {
+    "semi": [2, "always"]
+  },
   "env": {
     "vue/setup-compiler-macros": true
   }
-}
+};

3a3ce6a7ca0cbfe05f56137f3efe19e111ecae5e
Henry Lee <Henry840220@gmail.com>
2022-05-17 19:11:31 +0800

    加入一個sha256 hash的js

---
+++ src/tools/crypto.js
@@ -0,0 +1,13 @@
+export default {
+    async digestMessage(message) {
+        // encode as (utf-8) Uint8Array
+        const msgUint8 = new TextEncoder().encode(message);
+        // hash the message
+        const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+        // convert buffer to byte array
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        // convert bytes to hex string
+        const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+        return hashHex;
+    }
+};

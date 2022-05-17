967bb1624e6585d650dcbf45bce8ddde7d503b1c
Henry Lee <Henry840220@gmail.com>
2022-05-17 19:13:51 +0800

    首頁增加清除token，個人頁面增加顯示資料

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,18 @@
-<script setup></script>
+<script setup>
+
+function Logout() {
+  localStorage.removeItem('token');
+}
+
+</script>
 
 <template>
   home page
+
+  <button
+    class="btn btn-lg btn-primary"
+    @click="Logout"
+  >
+    Log out
+  </button>
 </template>
--- src/views/PersonalInfo.vue
+++ src/views/PersonalInfo.vue
@@ -1,5 +1,28 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import userService from '../services/userService';
+
+const user = ref(null);
+const done = ref(false);
+
+userService.getUser()
+           .then((response) => {
+             if (response.status === 200) {
+               user.value = response.data;
+               done.value = true;
+             }
+           });
+</script>
 
 <template>
   personal info
+  <table v-if="done">
+    <tr
+      v-for="(value, key) in user"
+      :key="key"
+    >
+      <td>{{ key }}</td>
+      <td>{{ value }}</td>
+    </tr>
+  </table>
 </template>

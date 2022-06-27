839cf9146edcf4bede66349625249a4cb809cee0
Henry Lee <Henry840220@gmail.com>
2022-06-27 14:46:17 +0800

    移除多餘的style

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,6 @@ const alreadyLogin = computed(() => {
   return route.path.includes('login') || route.path.includes('register');
 });
 
-
 </script>
 
 <template>
@@ -32,19 +31,3 @@ const alreadyLogin = computed(() => {
     <RouterView />
   </BaseLayout>
 </template>
-
-<style>
-  .bd-placeholder-img {
-    font-size: 1.125rem;
-    text-anchor: middle;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    user-select: none;
-  }
-
-  @media (min-width: 768px) {
-    .bd-placeholder-img-lg {
-      font-size: 3.5rem;
-    }
-  }
-</style>
